1a251ef4f6d01ae4e9a96081102ae480569580f0
JonathanLopez42 <jll3888@rit.edu>
2021-11-02 16:08:58 -0400

    classwork

--- activities.py
+++ activities.py
@@ -126,7 +126,7 @@ def minus (a_set, b_set):
     for value in b_set:
         if value in a_set:
             a_set.remove(value)
-            
+
 def names():
     names = ()
     names["J"] = "John"
@@ -139,7 +139,137 @@ def print_dict(dictionary):
     for key in dictionary:
         value = dictionary[key]
         print(key)
+
+
+def find_maximum(a_dict):
+    max_key = None
+    max_value = 0
+
+    for key in a_dict:
+        value = a_dict[key]
+
+        if value > max_value:
+            max_value = value
+            max_key = key
+    
+    return max_key, max_value
+
+def hashes():
+
+    print(hash("Hello world"))
+
+    hashcode1 = hash("Hello World!")
+    hashcode2 = hash("Hello World?")
+
+    print(hashcode1)
+    print(hashcode2)
+
+
+def collisions(filename,length,hash_func=hash):
+
+    an_array = arrays.Array(length, None)
+
+    file = open(filename)
+
+    with open(filename) as file:
+
+        count = 0
+
+        for line in file:
+            line = line.strip()
+
+
+
+            if line == "":
+
+                continue
+
+        hash_code = hash_func(line)
+
+        index = hash_code % length
+
+        if an_array[index] is None:
+            an_array[index] = line
+            count += 1
+        else:
+            return count
+
+
+    return count
+def make_myset(capacity, hash_func= hash):
+
+    table = [[] for _ in range(capacity)]
+
+    return (hash_func, table)
+
+
+
+def add(myset, element):
+
+    has_func = myset[0]
+    table = myset[1]
+
+    capacity = len(table)
+
+    index = has_func(element) % capacity
+
+    row = table[index]
+
+    for entry in row:
+
+        if entry == element:
+            return
+
+    table[index].append(element)
+
+
+def contains(myset, element):
+
+    hash_func = myset[0]
+    table = myset[1]
+    capacity = len(table)
+
+    index = hash_func(element) % capacity
+
+    row = table[index]
+
+    for elt in row:
+        if elt == element:
+            return True
+    return False
+
+
+def ascii_cides(a_string):
+    for char in a_string:
+        print(char, ":", ord(char), sep= "")
+    
+
+def string_hash(a_string):
+
+    max_aschii = -1
+
+    for char in a_string:
+        char_aschii = ord(char)
+        if char_aschii > max_aschii:
+            max_aschii = char_aschii
+    return max_aschii * len(a_string)
+
+def string_hash_betterer(a_string):
+    current = 0
+    for char in a_string:
+        current = current + ord(char) ** 3
+
+    return current
+
+
 def main():
+
+    myset = make_myset(7)
+    print(myset)
+
+    hashes()
+
+    find_maximum()
     size = 5000
     print("fill array:", fill_array(size), "seconds")
     print("fill array:", fill_list(size), "seconds")
