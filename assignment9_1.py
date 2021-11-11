cf8cdf3f2f4d58913009ad9d4029760058034f87
JonathanLopez42 <jll3888@rit.edu>
2021-11-10 22:09:13 -0500

    homework

---
+++ assignment9_1.py
@@ -0,0 +1,34 @@
+import time
+
+def in_place_reverse(a_list):
+    a_list = []
+    while a_list:
+        a_list.insert(len(a_list), a_list.pop())
+
+        print(a_list)
+        break
+def my_slice(a_list,start,stop,step=1):
+    
+    
+    
+    list = a_list[start:stop:step]
+    
+    list = [0,10,20,30,40,50]
+
+    print(list)
+   
+
+    
+
+
+
+def main():
+    """
+    a_lis = [1, 2, 3, 4, 5, 6]
+
+    in_place_reverse(a_lis)"""
+
+    a_list=[]
+    my_slice(a_list,2,4)
+
+main()
